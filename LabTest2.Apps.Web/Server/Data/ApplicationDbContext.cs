fb178111f63ad23265b35214b084490ec87a213c
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-14 04:00:36 +0600

    AB#17, AB#18, : Created ASP.NET Core Hosted Blazor WASM Project to host both API project and WASM static web and added them to the solution

---
+++ LabTest2.Apps.Web/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using IdentityServer4.EntityFramework.Options;
+
+using LabTest2.Apps.Web.Server.Models;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace LabTest2.Apps.Web.Server.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(
+		    DbContextOptions options,
+		    IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
