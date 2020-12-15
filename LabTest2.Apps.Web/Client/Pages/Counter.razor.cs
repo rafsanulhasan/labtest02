546327f77521b9414c22ee77c367c2db48b51e1f
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-15 10:44:33 +0600

    AB#22, AB#23: Configured RxUI and used Rx.NET to apply reactive style of programming

---
+++ LabTest2.Apps.Web/Client/Pages/Counter.razor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using LabTest2.Apps.Web.Shared.ViewModels;
+
+using Microsoft.AspNetCore.Components.Web;
+
+using ReactiveUI;
+using ReactiveUI.Blazor;
+
+using Syncfusion.Blazor.Buttons;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using System.Reactive.Disposables;
+
+namespace LabTest2.Apps.Web.Client.Pages
+{
+	public partial class Counter
+		: ReactiveComponentBase<CounterViewModel>
+	{
+		private readonly CompositeDisposable _disposables;
+		public SfButton IncreaseButton { get; set; }
+		public bool IsIncreaseButtonDisabled { get; private set; }
+
+		private void OnIncrease(MouseEventArgs _)
+			=> ViewModel
+				.IncreaseCommand
+				.Execute()
+				.Subscribe()
+				.DisposeWith(_disposables);
+
+		public Counter()
+		{
+			ViewModel = new CounterViewModel();
+			_disposables = new CompositeDisposable();
+			ViewModel.DisposeWith(_disposables);
+			this
+				.WhenAnyValue(v => v.ViewModel)
+				.Where(vm => vm != null)
+				.Select(vm =>
+				    Observable
+					    .FromEvent<PropertyChangedEventHandler, Unit>(
+						    handler =>
+						    {
+							    void Handler(object sender, PropertyChangedEventArgs e)
+								    => handler(Unit.Default);
+							    return Handler;
+						    },
+						    eh => vm.PropertyChanged += eh,
+						    eh => vm.PropertyChanged -= eh
+					    )
+				)
+				.Switch()
+				.Do(_ => InvokeAsync(StateHasChanged))
+				.Subscribe()
+				.DisposeWith(_disposables)
+				;
+		}
+
+		protected override void OnAfterRender(bool firstRender) 
+			=> IncreaseButton.DisposeWith(_disposables);
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (disposing)
+			{
+				_disposables?.Dispose();
+			}
+		}
+	}
+}
---
+++ LabTest2.Apps.Web/Shared/ViewModels/CounterViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+using ReactiveUI;
+
+namespace LabTest2.Apps.Web.Shared.ViewModels
+{
+	public class CounterViewModel
+		: ReactiveObject, IDisposable
+	{
+		private readonly IObservable<bool> _canNotIncrease;
+		private readonly CompositeDisposable _disposables;
+		private bool _disposedValue;
+		private int _count;
+		private bool _isIncreaseButtonDisabled;
+
+		public ReactiveCommand<Unit, Unit> IncreaseCommand { get; init; }
+
+		public int Count
+		{
+			get => _count;
+			private set => this.RaiseAndSetIfChanged(ref _count, value);
+		}
+		public bool IsIncreaseButtonDisabled
+		{
+			get => _isIncreaseButtonDisabled;
+			set => this.RaiseAndSetIfChanged(ref _isIncreaseButtonDisabled, value);
+		}
+
+		public CounterViewModel()
+		{
+			_disposables = new CompositeDisposable();
+
+			_canNotIncrease = this
+							.WhenAnyValue(vm => vm.Count)
+							.Select(c => c > 5);
+
+			_canNotIncrease
+				.Subscribe(v => IsIncreaseButtonDisabled = v)
+				.DisposeWith(_disposables);
+
+			IncreaseCommand = ReactiveCommand.Create(
+				() => Increment(),
+				_canNotIncrease
+			)
+			.DisposeWith(_disposables)
+			;
+
+		}
+
+		public void Decrement(int number = 1)
+			=> Count -= number;
+
+		public void Increment(int number = 1)
+			 => Count += number;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposedValue)
+			{
+				if (disposing)
+				{
+					_disposables?.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+
+				_disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~CounterViewModel()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
