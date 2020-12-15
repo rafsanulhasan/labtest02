db7d5665ad1fadc80507f5c5c97a1b7007b12f5c
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-15 10:48:17 +0600

    Created suppression file and added some code to the suppression list to avoid error in static code analysis

---
+++ LabTest2.Apps.Web/Shared/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S125:Sections of code should not be commented out", Justification = "<Pending>", Scope = "member", Target = "~M:LabTest2.Apps.Web.Shared.ViewModels.CounterViewModel.Dispose")]
+[assembly: SuppressMessage("Info Code Smell", "S1135:Track uses of \"TODO\" tags", Justification = "<Pending>", Scope = "member", Target = "~M:LabTest2.Apps.Web.Shared.ViewModels.CounterViewModel.Dispose")]
+[assembly: SuppressMessage("Info Code Smell", "S1135:Track uses of \"TODO\" tags", Justification = "<Pending>", Scope = "member", Target = "~M:LabTest2.Apps.Web.Shared.ViewModels.CounterViewModel.Dispose(System.Boolean)")]
