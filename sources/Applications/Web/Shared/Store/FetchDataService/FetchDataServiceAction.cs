ca1d8b245295149f83577bb227435372cea0ea86
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-20 05:46:41 +0600

    Implemented redux store in API

---
+++ sources/Applications/Web/Shared/Store/FetchDataService/FetchDataServiceAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+
+using LabTest2.Apps.Web.Shared.DTOs;
+
+namespace LabTest2.Apps.Web.Shared.Store.FetchDataService
+{
+	public record FetchDataServiceAction();
+	public record FetchDataServiceResultAction
+	{
+		public IImmutableList<WeatherForecastDTO> WeatherForecasts { get; init; }
+		public bool IsLoading { get; init; }
+
+		public FetchDataServiceResultAction(
+			IImmutableList<WeatherForecastDTO> weatherForecasts,
+			bool isLoading
+		)
+		{
+			WeatherForecasts = weatherForecasts;
+			IsLoading = isLoading;
+		}
+	}
+}
---
+++ sources/Applications/Web/Shared/Store/FetchDataService/FetchDataServiceState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+
+using LabTest2.Apps.Web.Shared.DTOs;
+
+namespace LabTest2.Apps.Web.Shared.Store.FetchDataService
+{
+	public record FetchDataServiceState(
+		IImmutableList<WeatherForecastDTO> WeatherForecasts,
+		bool IsLoading
+	) : StateBase;
+}
