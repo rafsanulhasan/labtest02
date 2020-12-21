910abdc86f72d336a704c97ab6753280c5e4c95a
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-21 07:23:48 +0600

    AB#25: updated DTOto support graphql

--- sources/Applications/Web/Shared/DTOs/WeatherForecastDTO.cs
+++ sources/Applications/Web/Shared/DTOs/WeatherForecastDTO.cs
@@ -4,13 +4,17 @@ namespace LabTest2.Apps.Web.Shared.DTOs
 {
 	public class WeatherForecastDTO
 	{
+		private int _temperatureF;
 		public DateTime Date { get; set; }
 
 		public int TemperatureC { get; set; }
 
-		public string Summary { get; set; }
+		public string Summary { get; set; } = string.Empty;
 
-		public int TemperatureF 
-			=> 32 + (int)(TemperatureC / 0.5556);
+		public int TemperatureF
+		{
+			get => 32 + (int)(TemperatureC / 0.5556);
+			set => _temperatureF =  32 + (int)(TemperatureC / 0.5556);
+		}
 	}
 }
