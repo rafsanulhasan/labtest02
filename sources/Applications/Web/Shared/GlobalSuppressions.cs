900a7dffd8ac44395ccf5c7ba19a843373a4cdc8
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-21 07:28:14 +0600

    AB#25: supprresed message

--- sources/Applications/Web/Shared/GlobalSuppressions.cs
+++ sources/Applications/Web/Shared/GlobalSuppressions.cs
@@ -1,5 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿using System.Diagnostics.CodeAnalysis;
+
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:LabTest2.Apps.Web.Shared.GraphTypes.Subscription.OnForecasted(LabTest2.Apps.Web.Shared.Services.IWeatherForecastService,System.Threading.CancellationToken)~System.Threading.Tasks.ValueTask{System.Collections.Generic.IAsyncEnumerable{LabTest2.Apps.Web.Shared.DTOs.WeatherForecastDTO}}")]
 
