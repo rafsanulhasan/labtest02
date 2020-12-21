4f806bcce310ccb77bdcd83a59af0b93b350dbfa
rafsanul Hasan <rafsanulhasan@outlook.com>
2020-12-21 07:23:11 +0600

    AB#25: used service

--- sources/Applications/Web/Server/Controllers/WeatherForecastController.cs
+++ sources/Applications/Web/Server/Controllers/WeatherForecastController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using LabTest2.Apps.Web.Shared.DTOs;
+﻿using LabTest2.Apps.Web.Shared.DTOs;
+using LabTest2.Apps.Web.Shared.Services;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
+using System.Collections.Generic;
 
 namespace LabTest2.Apps.Web.Server.Controllers
 {
@@ -16,30 +15,21 @@ namespace LabTest2.Apps.Web.Server.Controllers
 	public class WeatherForecastController
 		: ControllerBase
 	{
-		private static readonly string[] SUMMARIES = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
-
 		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly IWeatherForecastService _forecastService;
 
 		public WeatherForecastController(
-			ILogger<WeatherForecastController> logger
+			ILogger<WeatherForecastController> logger,
+			IWeatherForecastService forecastService
 		)
-			=> _logger = logger;
+		{
+			_logger = logger;
+			_forecastService = forecastService;
+		}
 
 		[AllowAnonymous]
 		[HttpGet]
-		public IEnumerable<WeatherForecastDTO> Get()
-		{
-			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecastDTO
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = SUMMARIES[rng.Next(SUMMARIES.Length)]
-			})
-			.ToArray();
-		}
+		public IAsyncEnumerable<WeatherForecastDTO> Get() 
+			=> _forecastService.GetForecasts();
 	}
 }
